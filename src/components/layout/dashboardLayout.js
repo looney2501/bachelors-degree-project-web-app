6a5342518a16c3582b7ad7f55674a16fa91d4413
Mircea-George Cozarev <mircea2501@gmail.com>
2023-06-12 22:30:03 +0300

    Remove loading screen

--- src/components/layout/dashboardLayout.js
+++ src/components/layout/dashboardLayout.js
@@ -1,23 +1,13 @@
 import Sidebar from './sidebar'
-import LoadingScreen from '../loading/LoadingScreen'
-import { useSelector } from 'react-redux'
 
 const dashboardLayout = ChildComponent => props => {
-  const isLoading = useSelector(state => state.auth.isLoading)
-
   return (
-    <>
-      {isLoading ? (
-        <LoadingScreen/>
-      ) : (
-        <div className="d-flex" id="wrapper">
-          <Sidebar/>
-          <div className="main" id="page-content-wrapper">
-            <ChildComponent {...props} />
-          </div>
-        </div>
-      )}
-    </>
+    <div className="d-flex" id="wrapper">
+      <Sidebar/>
+      <div className="main" id="page-content-wrapper">
+        <ChildComponent {...props} />
+      </div>
+    </div>
   )
 }
 
